ac41e4e74877c0ebc47ba511ea6f8c2d3fb2d165
chervonyi <yuri.ternopil@gmail.com>
2019-03-12 19:49:28 -0500

    Add BlackScreenViewController

---
+++ Decision/Decision/Views/BlackScreenViewController.swift
@@ -0,0 +1,35 @@
+//
+//  BlackScreenViewController.swift
+//  Decision
+//
+//  Created by Yuri Chervonyi on 3/12/19.
+//  Copyright © 2019 CHRGames. All rights reserved.
+//
+
+import UIKit
+
+class BlackScreenViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
